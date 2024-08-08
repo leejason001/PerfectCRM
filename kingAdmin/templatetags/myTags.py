754d8a378b3f168403c51f6b98f1afaf0474c957
leejason <13131412952@163.com>
2024-08-08 16:01:17 +0800

    加入判断筛选条件是否用了的判断

--- kingAdmin/templatetags/myTags.py
+++ kingAdmin/templatetags/myTags.py
@@ -24,8 +24,9 @@ def displayFilter(filter_column, configTableClass, filter_conditions):
         columnObj = configTableClass.model._meta.get_field( filter_column )
         for choice in columnObj.get_choices():
             selected = ''
-            if str(choice[0]) == filter_conditions.get(filter_column):
-                selected = 'selected'
+            if filter_column in filter_conditions:
+                if str(choice[0]) == filter_conditions.get(filter_column):
+                    selected = 'selected'
             selectDom += '<option value="%s" %s>%s</option>' % (choice[0], selected, choice[1])
     except AttributeError as e:
         selectDom = '<select name="%s__gte">' % filter_column
@@ -40,8 +41,9 @@ def displayFilter(filter_column, configTableClass, filter_conditions):
         for choice in dateChoicesList:
             selected = ''
             choice[0] = '' if not choice[0] else '%s-%s-%s'%(choice[0].year, choice[0].month, choice[0].day)
-            if str(choice[0]) == filter_conditions.get('%s__gte'%filter_column):
-                selected = 'selected'
+            if '%s__gte'%filter_column in filter_conditions:
+                if str(choice[0]) == filter_conditions.get('%s__gte'%filter_column):
+                    selected = 'selected'
             selectDom += '<option value="%s" %s>%s</option>'%(choice[0], selected, choice[1])
 
     selectDom += '</select>'
