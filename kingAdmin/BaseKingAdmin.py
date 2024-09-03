55890647d3ee684c7aa1c2963c75969e9556d214
leejason <13131412952@163.com>
2024-09-03 16:05:17 +0800

    初步完成“GO批量删除

--- kingAdmin/BaseKingAdmin.py
+++ kingAdmin/BaseKingAdmin.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import json
+
 
 
 class BaseKingAdmin(object):
@@ -11,7 +13,11 @@ class BaseKingAdmin(object):
     def __init__(self):
         self.actions.extend(self.default_actions)
 
-    def delelteSelected(self, request, appName, tableName, querysets):
+    def delelteSelected(self, request, appName, tableName, querysets, rowsQuerySet, sorted_column):
         configTableClass = self
         theRowSet = configTableClass.model.objects.filter( id__in=querysets )
+        if "POST" == request.method:
+            if "yes" == request.POST.get("delete_confirm"):
+                theRowSet.delete()
+                return redirect("/kingAdmin/%s/%s"%(appName, tableName))
         return render( request, 'tableDelete.html', locals())
